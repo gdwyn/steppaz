a0a05b4e1c2498c1964832dde3f2b05716ed5d52
Godwin Ebri <godwinnebri@gmail.com>
2025-03-05 23:00:34 +0100

    chore: add folder structure

--- steppaz/App/steppazApp.swift
+++ steppaz/App/steppazApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct steppaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
---
+++ steppaz/Core/Splash/View/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  steppaz
+//
+//  Created by Godwin IE on 05/03/2025.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
